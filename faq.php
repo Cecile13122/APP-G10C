49c11c64c7ec0f741c762905639cd545164e6d08
Cecile13122 <cecile.meynieux@free.fr>
2021-04-29 00:50:32 +0200

    creation faq, modification faq.php, requestsFaq.php, planDuSite.html et style.css

---
+++ faq.php
@@ -0,0 +1,40 @@
+<?php
+require_once('requestsFaq.php');
+$faq=recuperation_faq();
+$themes= $faq[0];
+$donne_faq= $faq[1];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" name="viewport"/>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <title>Army's Test Company</title>
+    </head>
+    <body>
+      <div class="navbar">
+        <img src="ATC_v200.png" alt="Logo ATC">
+        <a href="resultats.html" >Résultats</a>
+        <a href="test.html">Test</a>
+        <a class="active" href="faq.php">FAQ</a>
+        <a href="contact.html">Contact</a>
+        <a href="#recherche"><textarea name="informations" rows="1" cols="20">Recherche</textarea></a>
+        <a class ="profil" href="#profil">Luc Colin</a>
+      </div>
+      <div class="main">
+          <?php foreach ($themes as $theme): ?>
+        <h2><?=$theme['theme']?></h2>
+        <div class= "themeFAQ">
+            <?php foreach ($donne_faq as $donnee):
+            if ($donnee["theme"] == $theme['theme']){?>
+            <p><h3><?=$donnee['question'] ?></h3><br><?=$donnee['reponse'] ?><br></p>
+            <?php }
+           endforeach;?>
+        </div>
+          <?php endforeach;?>
+      </div>
+      <div class="footer">
+        <a href="CGU.php">CGU</a>  |  <a href="mentionsLegales.php">Mentions Légales</a>  |  <a href="planDuSite.html">Plan du site</a>
+      </div>
+    </body>
+</html>
--- requestsFaq.php
+++ requestsFaq.php
@@ -5,17 +5,15 @@ function recuperation_faq(){
     $bdd=connect_bdd();
     $requete_faq=$bdd->prepare('SELECT * FROM faq ORDER BY theme');
     $requete_faq->execute();
+    $info_theme=[];
     $info_faq=[];
     $requete_theme=$bdd->prepare('SELECT DISTINCT theme FROM faq');
     $requete_theme->execute();
     while ($donnee_theme= $requete_theme->fetch()){
-        $info_faq[]=$donnee_theme;
+        $info_theme[]=$donnee_theme;
     }
     while ($donne_faq=$requete_faq->fetch()){
-        foreach ($info_faq as $theme){
-            if($donne_faq['theme']==$theme){
-                array_push($info_faq[$theme]);
-            }
-        }
+        $info_faq[]=$donne_faq;
     }
+    return array($info_theme, $info_faq);
 }
