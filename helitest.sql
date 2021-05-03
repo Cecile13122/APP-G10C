13ff01891ce371690737018eaaf6bdb2da77f649
Cecile13122 <cecile.meynieux@free.fr>
2021-05-03 21:13:01 +0200

    modification requestsFaq.php requestsAdministrateur.php, requestsCandidat.php, requestsRecruteur.php, requestsSession.php et mise à jour de la bdd  helitest.sql

--- helitest.sql
+++ helitest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le :  mar. 20 avr. 2021 à 18:18
+-- Généré le :  lun. 03 mai 2021 à 19:11
 -- Version du serveur :  10.3.14-MariaDB
 -- Version de PHP :  7.2.18
 
@@ -31,11 +31,18 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `administrateur`;
 CREATE TABLE IF NOT EXISTS `administrateur` (
   `mail_admin` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `firstname` varchar(128) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(128) NOT NULL,
   `mdp` varchar(64) NOT NULL,
   PRIMARY KEY (`mail_admin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `administrateur`
+--
+
+INSERT INTO `administrateur` (`mail_admin`, `prenom`, `nom`, `mdp`) VALUES
+('cecile.meynieux@free.fr', 'Cecile', 'Meynieux', '$2y$10$tMvDpQMUfUdscJZ3U1VF9e838zyaKdXTkGT1Yp7il6EsaM41apjHu');
 
 -- --------------------------------------------------------
 
@@ -56,6 +63,15 @@ CREATE TABLE IF NOT EXISTS `candidat` (
   PRIMARY KEY (`mail_candidat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `candidat`
+--
+
+INSERT INTO `candidat` (`mail_candidat`, `nom`, `prenom`, `mdp`, `date_naissance`, `numero_tel`, `genre`, `code_postal`) VALUES
+('pconde@isep.fr', 'Conde-CESPESDES', 'Patricia', '$2y$10$CbZF.PyXgQ5EGcfRUQ0IL.e1rTjqY6pTTAOTVEXXDivAWYylAwYvi', '2000-10-11', '0633100256', 'F', 75006),
+('pv@gmail.com', 'Vidor', 'Paul', '$2y$10$d0DlgNacFN.NgMaAPV3.9.XmpAe4U9LiFTAOAQB0LT8C8w5EZe1vS', '1997-06-27', '0633100256', 'M', 75467),
+('R.h@free.fr', 'Henry', 'Robin', '$2y$10$ou2LJ/qekAy.0002WnKa9.9wjQFrYRK.MJmIl3v2cN2h8lEf49.bK', '1999-02-28', '0834543212', 'M', 13122);
+
 -- --------------------------------------------------------
 
 --
@@ -67,9 +83,24 @@ CREATE TABLE IF NOT EXISTS `faq` (
   `id_faq` int(11) NOT NULL AUTO_INCREMENT,
   `theme` varchar(128) NOT NULL,
   `question` varchar(255) NOT NULL,
-  `answer` text NOT NULL,
+  `reponse` text NOT NULL,
   PRIMARY KEY (`id_faq`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `faq`
+--
+
+INSERT INTO `faq` (`id_faq`, `theme`, `question`, `reponse`) VALUES
+(1, 'Test', 'Où se déroulent les tests ?', 'Les test ne s\'effectuent qu\'en présentiel au Centre d\'Information et de Recrutement des Forces Armées (CIRFA) le plus proche de chez vous après une prise de rendez-vous avec un recruteur.'),
+(2, 'Test', 'Comment se déroule un test ?', 'Un test dure une quinzaine de minutes durant lesquels plusieurs épreuves auront lieu. Un boîtier électronique est utilisé pour faire un test.'),
+(3, 'Test', 'Quelles sont les épreuves ?', 'Il y a plusieurs types d\'épreuves qui visent à évaluer les reflex mais également la gestion du stress.'),
+(4, 'Test', 'Comment se préparer aux épreuves ?', 'Aucune préparation n’est requise afin de se préparer à ces épreuves. En effet, il s’agit de compétences innées. '),
+(5, 'Résultat', 'Pourquoi n’ai-je que les résultats aux différentes épreuves et pas le résultat final ? ', 'Le résultat final n’est communiqué qu’à la suite de la réception de tous les tests de candidats qui étaient prévus dans la session. Cependant, les résultats aux différentes épreuves sont communiqués instantanément au candidat. '),
+(6, 'Résultat', 'Puis-je repasser les épreuves en cas de non succès ?', 'Vous pouvez repasser jusqu’à trois fois les tests, en étant âgé de moins de 32 ans. En cas de nos succès aux différents tests lors de trois sessions, vous n’aurez plus la capacité de postuler pour devenir pilote d’hélicoptère.'),
+(7, 'Résultat', 'Comment sont définis les seuils de validation des épreuves ?', 'Les seuils sont définis par les valeurs standard attendu pour un pilote d\'hélicoptère. Cependant, si le nombre de candidat ayant réussi est supérieur au nombre de place disponible, le recruteur à la possibilité de remonter les seuils pour ne garder que les meilleurs de la session. Il n\'a pas la possibilité de les abaisser.'),
+(8, 'Informations personnelles', 'A quoi servent mes données personnelles ?', 'Les données personnelles sont stockées durant dix ans par l’armée. Cela permet d’effectuer des statistiques afin de s’adapter aux candidats et de mettre à jour régulièrement les tests. '),
+(9, 'Informations personnelles ', 'Quels sont mes droits ? ', 'Vous avez droits de demander une suppression de l’ensemble de vos données personnelles stockées numériquement comme le prévoit la loi. Ainsi, veuillez contacter le support, grâce à l’onglet « contact» afin de faire valoir vos droits. Une réponse vous sera adressé une fois que l’opération aura été effectuée. ');
 
 -- --------------------------------------------------------
 
@@ -80,11 +111,18 @@ CREATE TABLE IF NOT EXISTS `faq` (
 DROP TABLE IF EXISTS `recruteur`;
 CREATE TABLE IF NOT EXISTS `recruteur` (
   `mail_recruteur` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`mail_recruteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `recruteur`
+--
+
+INSERT INTO `recruteur` (`mail_recruteur`, `nom`, `prenom`, `mdp`) VALUES
+('marie.mottier@gmail.com', 'Mottier', 'Marie', '$2y$10$InE6xQzHXPcCuVP60vIIYe1Be.5vtv36NcK.zskE7a3FVGf8smsB6');
 
 -- --------------------------------------------------------
 
--- requestsFaq.php
+++ requestsFaq.php
@@ -17,3 +17,41 @@ function recuperation_faq(){
     }
     return array($info_theme, $info_faq);
 }
+
+function ajout_question($theme, $question, $reponse){
+    $bdd=connect_bdd();
+    $requete=$bdd->prepare('INSERT INTO faq (id_faq, theme, question, reponse) VALUES (NULL, :theme, :question, :reponse)');
+    $requete->execute(array(
+        'theme'=>$theme,
+    'question'=>$question,
+    'reponse'=>$reponse
+    ));
+}
+
+function affiche_faq(){
+    $bdd=connect_bdd();
+    $requete=$bdd->prepare('SELECT * FROM faq ORDER BY theme');
+    $requete->execute();
+    $faq=[];
+    while($donnee_faq = $requete->fetch()){
+        $faq[]=$donnee_faq;
+    }
+    return $faq;
+}
+
+function effacer_question($id){
+    $bdd=connect_bdd();
+    $requete=$bdd->prepare('DELETE faq FROM faq WHERE id_faq=?');
+    $requete->execute(array($id));
+}
+
+function modifier_question($id, $theme, $question, $reponse){
+    $bdd=connect_bdd();
+    $requete=$bdd->prepare('UPDATE faq SET theme=:theme, question=:question, reponse=:reponse WHERE id_faq=:id');
+    $requete->execute(array(
+        'theme'=> $theme,
+        'question'=>$question,
+        'reponse'=>$reponse,
+        'id'=>$id));
+}
+
