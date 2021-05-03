721b357d197dbdb5cb8c919a6b140dcaa4880948
mariembo <cheesecakemarie@gmail.com>
2021-05-03 21:30:06 +0200

    ,j

--- accueil.php
+++ accueil.php
@@ -12,11 +12,8 @@ elseif ($_SESSION['role']=="recruteur"){
 ?>
   <div class="main">
     <h1>Bienvenue</h1>
-    <p>Sur ce site vous trouverez vos résultats aux différents tests. Une fois que tous les candidats de votre session auront passé les tests vous saurez si vous êtes admis pour la prochaine phase de recrutement.</p>
-
-
-<br><br>
-  <p>Il est important de mesurer les réflexes d’un futur pilote d'hélicoptère. En effet, tout le monde n’a pas les mêmes aptitudes, et dans le cadre des réflexes et du temps de réaction, la part de l’innée est très importante et l’entraînement ne permet pas de rattraper toutes les lacunes. Il est donc très important d’écarter grâce aux tests psychotechniques ceux qui ne pourront jamais obtenir les aptitudes requises.  Pour cela, on va par exemple mesurer la capacité du candidat à reconnaître une tonalité et à la répéter. Cette mesure donnera par exemple un aperçu de la validation opérationnelle d’un candidat face à des bruits extérieurs ayant des fréquences différentes, que l’on peut assimiler à différentes sonneries d’alarmes. On testera aussi ses reflex à l’aide de stimuli sonore ou visuel. La gestion du stress est aussi un élément important dans la vie d’un militaire, c’est pourquoi différents tests permettent de la mesurer.</p>
+    <p class="milieu">Sur ce site vous trouverez vos résultats aux différents tests. Une fois que tous les candidats de votre session auront passé les tests vous saurez si vous êtes admis pour la prochaine phase de recrutement.<br><br>
+Il est important de mesurer les réflexes d’un futur pilote d'hélicoptère. En effet, tout le monde n’a pas les mêmes aptitudes, et dans le cadre des réflexes et du temps de réaction, la part de l’innée est très importante et l’entraînement ne permet pas de rattraper toutes les lacunes. Il est donc très important d’écarter grâce aux tests psychotechniques ceux qui ne pourront jamais obtenir les aptitudes requises.  Pour cela, on va par exemple mesurer la capacité du candidat à reconnaître une tonalité et à la répéter. Cette mesure donnera par exemple un aperçu de la validation opérationnelle d’un candidat face à des bruits extérieurs ayant des fréquences différentes, que l’on peut assimiler à différentes sonneries d’alarmes. On testera aussi ses reflex à l’aide de stimuli sonore ou visuel. La gestion du stress est aussi un élément important dans la vie d’un militaire, c’est pourquoi différents tests permettent de la mesurer.</p>
 
 </div>
 <div class="footer">
--- requestsConnexion.php
+++ requestsConnexion.php
@@ -3,7 +3,7 @@
 function connect_bdd() {
 // Connexion a la base de donnée
     try {
-        $bdd = new PDO ('mysql:host=localhost;port=3307; dbname=helitest; charset=utf8', 'root', '');
+        $bdd = new PDO ('mysql:host=localhost;port=8889; dbname=helitest; charset=utf8', 'root', 'root');
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $bdd;
 
@@ -78,21 +78,3 @@ function modifier_mot_de_passe($mot_de_passe, $mail, $role){
             'mail' => $mail));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
