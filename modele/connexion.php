78a1a2f1dde951a78d3a97815ccf079f2d2fd7a5
mariembo <cheesecakemarie@gmail.com>
2021-05-31 19:42:45 +0200

    header admin good

--- modele/connexion.php
+++ modele/connexion.php
@@ -1,8 +1,8 @@
 <?php
 // Connexion a la base de donnée
     try {
-        //$bdd = new PDO ('mysql:host=localhost;port=8889; dbname=helitest; charset=utf8', 'root', 'root');
-        $bdd = new PDO ('mysql:host=localhost;port=3307; dbname=helitest; charset=utf8', 'root', '');
+        $bdd = new PDO ('mysql:host=localhost;port=8889; dbname=helitest; charset=utf8', 'root', 'root');
+        //$bdd = new PDO ('mysql:host=localhost;port=3307; dbname=helitest; charset=utf8', 'root', '');
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $bdd;
 
@@ -12,8 +12,8 @@
 
 function connect_bdd(){
   try {
-      //$bdd = new PDO ('mysql:host=localhost;port=8889; dbname=helitest; charset=utf8', 'root', 'root');
-      $bdd = new PDO ('mysql:host=localhost;port=3307; dbname=helitest; charset=utf8', 'root', '');
+      $bdd = new PDO ('mysql:host=localhost;port=8889; dbname=helitest; charset=utf8', 'root', 'root');
+      //$bdd = new PDO ('mysql:host=localhost;port=3307; dbname=helitest; charset=utf8', 'root', '');
       $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $bdd;
 
--- vues/header.administrateur.php
+++ vues/header.administrateur.php
@@ -8,27 +8,28 @@
   <body>
     <div class="header">
       <a href="index.php?cible=utilisateurs&fonction=afficher_page&page=accueil"><img src="./images/ATC_v200.png" alt="Logo ATC"></a>
+      <div class="profil">
+        <a class="modif_profil"><h2><?=$_SESSION['prenom']." ". strtoupper($_SESSION['nom'])."</h2>".strtoupper($role)?><br>Mon profil</a>
+        <div class = "sous_profil">
+          <a href="index.php?cible=utilisateurs&fonction=modification_mdp">Modification mot de passe</a>
+          <a href="index.php?cible=utilisateurs&fonction=deconnexion"> Déconnexion </a>
+        </div>
+      </div>
       <div class="navbar">
         <a href="index.php?cible=faq&fonction=afficher_faq">FAQ</a>
         <div class="header_utilisateurs">
           <a class="modif_header_utilisateurs">Utilisateurs</a>
           <div class="sous_header_utilisateurs">
-            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=candidat">Candidats</a>
-            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=recruteur">Recruteurs</a>
-            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=administrateur">Administrateurs</a>
+            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=candidat">Candidats</a><br>
+            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=recruteur">Recruteurs</a><br>
+            <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=administrateur">Administrateurs</a><br>
           </div>
         </div>
         <a href="index.php?cible=utilisateurs&fonction=afficher_page&page=contact">Contact</a>
-        <div class="recherche">
-          <input type="text" name="recherche" placeholder="Recherche">
-          <button type="submit"><img src="./images/loupe.png" alt="Loupe"></button>
-        </div>
+        <form class="recherche" method="post" action ="index.php?cible=utilisateurs&fonction=recherche">
+            <input type="text" name="recherche" placeholder="Recherche" required onchange="verificationNom(this.value, this.id)">
+            <button type="submit" class="loupe"><img src="./images/loupe.png" alt="Loupe" class="add"></button>
+        </form>
       </div>
-      <div class="profil">
-        <a class="modif_profil"><h2><?=$_SESSION['prenom']." ". strtoupper($_SESSION['nom'])."</h2>".strtoupper($role)?><br>Mon profil</a>
-        <div class = "sous_profil">
-          <a href="index.php?cible=utilisateurs&fonction=modification_mdp"> Modification mot de passe </a>
-          <a href="index.php?cible=utilisateurs&fonction=deconnexion"> Déconnexion </a>
-        </div>
-      </div>
+
     </div>
