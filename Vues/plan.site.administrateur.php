4d8217b4e8c18a9eefc2c59b4ea0880be1ab0cc1
mariembo <cheesecakemarie@gmail.com>
2021-06-01 12:55:45 +0200

    g

--- Vues/plan.site.administrateur.php
+++ Vues/plan.site.administrateur.php
@@ -1,11 +1,11 @@
 <div class="main">
-    <a href="index.php?cible=utilisateurs&fonction=accueil" class="link">Accueil</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=deconnexion" class="link">Déconnexion</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=modification_mdp" class="link">Modification du mot de passe</a><br>
-    <a href="index.php?cible=faq" class="link">Foire Aux Questions</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=candidat" class="link">Candidats</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=recruteur" class="link">Recruteurs</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=administrateur" class="link">Administrateurs</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=mentions_legales" class="link">Mentions Légales</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=cgu" class="link">Conditions Générales d'Utilisation</a><br>
+    <h1><a href="index.php?cible=utilisateurs&fonction=accueil" class="link">Accueil</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=deconnexion" class="link">Déconnexion</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=modification_mdp" class="link">Modification du mot de passe</a></h1>
+    <h1><a href="index.php?cible=faq" class="link">Foire Aux Questions</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=candidat" class="link">Candidats</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=recruteur" class="link">Recruteurs</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=afficher_utilisateurs&role_utilisateur=administrateur" class="link">Administrateurs</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=mentions_legales" class="link">Mentions Légales</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=cgu" class="link">Conditions Générales d'Utilisation</a></h1>
 </div>
--- Vues/plan.site.candidat.php
+++ Vues/plan.site.candidat.php
@@ -1,11 +1,11 @@
 <div class="main">
-    <a href="index.php?cible=utilisateurs&fonction=accueil" class="link">Accueil</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=deconnexion" class="link">Déconnexion</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=modification_profil" class="link">Modification du profil</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=modification_mdp" class="link">Modification du mot de passe</a><br>
-    <a href="index.php?cible=test&fonction=resultat" class="link">Résultats</a><br>
-    <a href="index.php?cible=faq" class="link">Foire Aux Questions</a><br>
-    <a href="index.php?cible=contact" class="link">Contact</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=mentions_legales" class="link">Mentions Légales</a><br>
-    <a href="index.php?cible=utilisateurs&fonction=cgu" class="link">Conditions Générales d'Utilisation</a><br>
+    <h1><a href="index.php?cible=utilisateurs&fonction=accueil" class="link">Accueil</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=deconnexion" class="link">Déconnexion</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=modification_profil" class="link">Modification du profil</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=modification_mdp" class="link">Modification du mot de passe</a></h1>
+    <h1><a href="index.php?cible=test&fonction=resultat" class="link">Résultats</a></h1>
+    <h1><a href="index.php?cible=faq" class="link">Foire Aux Questions</a></h1>
+    <h1><a href="index.php?cible=contact" class="link">Contact</a></h1>
+    <h1><a href="index.php?cible=utilisateurs&fonction=mentions_legales" class="link">Mentions Légales</a></h1>
+    </h1><a href="index.php?cible=utilisateurs&fonction=cgu" class="link">Conditions Générales d'Utilisation</a></h1>
 </div>
